Corrected colormap normalization Added snow change plot

plot_cape_cin.py
@@ -37,9 +37,9 @@ def main():
     dset[cape_cf_name] = dset[cape_cf_name].where(dset[cape_cf_name] >= 100)
 
     levels_cape = np.concatenate(
-        [np.arange(0.0, 3000.0, 100.0), np.arange(3000.0, 7000.0, 200.0)]
+        [np.arange(0.0, 3000.0, 100.0), np.arange(3000.0, 7200.0, 200.0)]
     )
-    cmap, norm = utils.get_colormap_norm("cape_wxcharts", levels=levels_cape)
+    cmap, norm = utils.get_colormap_norm("cape_wxcharts", levels=levels_cape, extend='max')
 
     _ = plt.figure(figsize=(figsize_x, figsize_y))
     ax = plt.gca()

plot_hsnow_change.py
@@ -2,8 +2,6 @@ from functools import partial
 from multiprocessing import Pool
 
 import matplotlib.pyplot as plt
-import metpy.calc as mpcalc
-import numpy as np
 
 import utils
 from definitions import (
@@ -18,30 +16,58 @@ from definitions import (
 args = utils.parse_arguments()
 debug = args.debug
 projection = args.projection
-variable_name = "t_2m"
+variable_name = "h_snow_change"
 output_dir = utils.set_output_dir(projection)
 
 if not debug:
     import matplotlib
+
     matplotlib.use("Agg")
+
 
 def main():
     logging.info(
         f"Plotting {variable_name} for projection {projection}. Writing images in {output_dir}"
     )
-    dset = utils.get_files_sfc(
-        vars=["T_2M"], projection=projection
+    dset = utils.get_files_sfc(vars=["H_SNOW"], projection=projection)
+    cf_var_name = utils.find_variable_by_grib_param_id(dset, 500045)
+    dset[cf_var_name] = dset[cf_var_name].metpy.convert_units("cm").metpy.dequantify()
+    hsnow = dset[cf_var_name] - dset[cf_var_name].isel(step=0)
+    hsnow = hsnow.where((hsnow > 0.25) | (hsnow < -0.25))
+    dset["hsnow_change"] = hsnow
+
+    levels_snow = (
+        -40,
+        -30,
+        -20,
+        -15,
+        -10,
+        -7,
+        -5,
+        -3,
+        -1,
+        1,
+        3,
+        5,
+        7,
+        10,
+        15,
+        20,
+        30,
+        40,
+        50,
+        75,
+        100,
+        150,
+        200,
     )
-    t2m_cf_name = utils.find_variable_by_grib_param_id(dset, 500011)
-    dset[t2m_cf_name] = dset[t2m_cf_name].metpy.convert_units("degC").metpy.dequantify()
 
-    levels_t2m = np.arange(-25, 45, 1)
-
-    cmap, norm = utils.get_colormap_norm("temp", levels_t2m, extend='both')
+    cmap, norm = utils.get_colormap_norm("snow_change", levels_snow, extend="both")
     _ = plt.figure(figsize=(figsize_x, figsize_y))
 
     ax = plt.gca()
-    _, x, y = utils.get_projection(dset, projection)
+    m, x, y = utils.get_projection(dset, projection)
+    m.arcgisimage(service="World_Shaded_Relief", xpixels=1500)
 
     # All the arguments that need to be passed to the plotting function
     args = dict(
@@ -50,12 +76,12 @@ def main():
         ax=ax,
         cmap=cmap,
         norm=norm,
-        levels_t2m=levels_t2m,
+        levels_snow=levels_snow,
     )
 
     logging.info("Pre-processing finished, launching plotting scripts")
     if debug:
-        plot_files(dset.isel(step=slice(0, 2)), **args)
+        plot_files(dset.isel(step=slice(-2, -1)), **args)
     else:
         # Parallelize the plotting by dividing into chunks and utils.processes
         dss = utils.chunks_dataset(dset, chunks_size)
@@ -68,7 +94,6 @@ def plot_files(dss, **args):
     first = True
     for step in dss["step"]:
         data = dss.sel(step=step).copy()
-        t2m_cf_name = utils.find_variable_by_grib_param_id(data, 500011)
         cum_hour = int(
             ((data["valid_time"] - data["time"]).dt.total_seconds() / 3600).item()
         )
@@ -81,34 +106,17 @@ def plot_files(dss, **args):
         cs = args["ax"].contourf(
             args["x"],
             args["y"],
-            data[t2m_cf_name],
+            data["hsnow_change"],
             extend="both",
             cmap=args["cmap"],
             norm=args["norm"],
-            levels=args["levels_t2m"],
-        )
-        density = 13
-        if projection == 'nord':
-            density = 8
-        elif projection == 'sud':
-            density = 7
-        elif projection == 'centro':
-            density = 5
-
-        vals = utils.add_vals_on_map(
-            ax=args["ax"],
-            var=data[t2m_cf_name],
-            x=args["x"],
-            y=args["y"],
-            cmap=args['cmap'],
-            norm=args['norm'],
-            density=density
+            levels=args["levels_snow"],
         )
 
         an_fc = utils.annotation_forecast(args["ax"], data["valid_time"].to_pandas())
         an_var = utils.annotation(
             args["ax"],
-            "Temperature@2m",
+            "Snow depth change [cm] since run beginning",
             loc="lower left",
             fontsize=6,
         )
@@ -118,7 +126,7 @@ def plot_files(dss, **args):
             cb = plt.colorbar(
                 cs,
                 orientation="horizontal",
-                label="Temperature [C]",
+                label="Snow height change [cm]",
                 pad=0.03,
                 fraction=0.04,
             )
@@ -132,10 +140,9 @@ def plot_files(dss, **args):
         utils.remove_collections(
             [
                 cs,
-                vals,
                 an_fc,
                 an_var,
-                an_run
+                an_run,
             ]
         )
 

plot_precip_acc.py
@@ -40,7 +40,7 @@ def main():
             np.arange(1100, 2100, 100),
         ]
     )
-    cmap, norm = utils.get_colormap_norm("prec_acc_wxcharts", levels=levels_precip)
+    cmap, norm = utils.get_colormap_norm("prec_acc_wxcharts", levels=levels_precip, extend='max')
 
     _ = plt.figure(figsize=(figsize_x, figsize_y))
     ax = plt.gca()
@@ -52,7 +52,7 @@ def main():
 
     logging.info("Pre-processing finished, launching plotting scripts")
     if debug:
-        plot_files(dset.isel(step=slice(0, 2)), **args)
+        plot_files(dset.isel(step=slice(-2, -1)), **args)
     else:
         # Parallelize the plotting by dividing into chunks and utils.processes
         dss = utils.chunks_dataset(dset, chunks_size)
@@ -84,6 +84,24 @@ def plot_files(dss, **args):
             norm=args["norm"],
             levels=args["levels_precip"],
         )
+    
+        density = 17
+        if projection == 'nord':
+            density = 10
+        elif projection == 'sud':
+            density = 9
+        elif projection == 'centro':
+            density = 7
+        vals = utils.add_vals_on_map(
+            ax=args["ax"],
+            var=data[tp_cf_name].where(data[tp_cf_name] > 50),
+            x=args["x"],
+            y=args["y"],
+            cmap=args['cmap'],
+            norm=args['norm'],
+            density=density,
+            fontsize=6
+        )
 
         an_fc = utils.annotation_forecast(args["ax"], data["valid_time"].to_pandas())
         an_var = utils.annotation(
@@ -111,7 +129,7 @@ def plot_files(dss, **args):
             plt.savefig(filename, **options_savefig)
 
         utils.remove_collections(
-            [cs, an_fc, an_var, an_run]
+            [cs, an_fc, an_var, an_run, vals]
         )
 
         first = False